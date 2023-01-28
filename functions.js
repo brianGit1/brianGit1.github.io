f75b4a59712cef3e5687ed90ac7a423a7c0d3bd9
Brian-Monteros <brian.monteros@outlook.com.ar>
2023-01-28 04:28:21 -0300

    save 1

---
+++ functions.js
@@ -0,0 +1,47 @@
+function changeColor1() {
+    var element = document.getElementById("text3");
+    element.style.color = "#f41f03";
+  }
+  function changeColor2() {
+    var element = document.getElementById("text3");
+    element.style.color = "#9eaaaf";
+  }
+  function changeColor3() {
+    var element = document.getElementById("text4");
+    element.style.color = "#f41f03";
+  }
+  function changeColor4() {
+    var element = document.getElementById("text4");
+    element.style.color = "#9eaaaf";
+  }
+  function changeColor5() {
+    var element = document.getElementById("text5");
+    element.style.color = "#f41f03";
+  }
+  function changeColor6() {
+    var element = document.getElementById("text5");
+    element.style.color = "#9eaaaf";
+  }
+  function changeColor7() {
+    var element = document.getElementById("text6");
+    element.style.color = "#f41f03";
+  }
+  function changeColor8() {
+    var element = document.getElementById("text6");
+    element.style.color = "#9eaaaf";
+  }
+  function changeColor9() {
+    var element = document.getElementById("text7");
+    element.style.color = "#f41f03";
+  }
+  function changeColor10() {
+    var element = document.getElementById("text7");
+    element.style.color = "#9eaaaf";
+  }
+  document.getElementById('pic').addEventListener("click", function() {
+    document.getElementById("age").style.display = "block";
+  })
+  document.getElementById('age').addEventListener("click", function() {
+    document.getElementById("age").style.display = "none";
+  })
+
